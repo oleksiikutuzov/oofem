2ab96e025fcf7194c3df86b655800ddc43a5dad7
Oleksii Kutuzov <KutuzoffLeha@users.noreply.github.com>
2020-07-23 01:22:55 +0200

    test classes

---
+++ oofem/src/test/TestArrow.java
@@ -0,0 +1,21 @@
+package test;
+
+import inf.v3d.obj.*;
+import inf.v3d.view.Viewer;
+
+public class TestArrow {
+
+	public static void main(String[] args) {
+		Arrow arrow = new Arrow();
+		
+		arrow.setPoint1(0, 0, 0);
+		arrow.setPoint2(1, 2, 0);
+		
+		Viewer v = new Viewer();
+		v.addObject3D(arrow);
+		
+		v.setVisible(true);
+
+	}
+
+}
