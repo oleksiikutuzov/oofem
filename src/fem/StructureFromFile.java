104e525cafff58c33da08407c54ed318ff6c0ab6
Oleksii Kutuzov <oleksii.kutuzov@icloud.com>
2022-01-04 22:50:34 +0100

    Update

--- src/fem/StructureFromFile.java
+++ src/fem/StructureFromFile.java
@@ -1,21 +1,17 @@
-package test;
+package fem;
 
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import fem.CSVReader;
-import fem.Structure;
-import fem.Visualizer;
-//import inf.v3d.view.Viewer;
+import inf.v3d.view.Viewer;
 
 public class StructureFromFile {
 
 	public static void main(String[] args) throws IOException {
 
 		// initialize Viewer and reader
-		// Viewer viewer = new Viewer();
+		Viewer viewer = new Viewer();
 
 		// define path to the model
 		String modelPath = "src/models/DomeTruss.csv";
@@ -24,9 +20,9 @@ public class StructureFromFile {
 		// create Structure by getting values from file
 		Structure struct = reader.getValues();
 		// initialize Visualizer
-		// Visualizer viz = new Visualizer(struct, viewer);
+		Visualizer viz = new Visualizer(struct, viewer);
 		// set Scales to values from file
-		// viz.transferScalesValues();
+		viz.transferScalesValues();
 
 		// set path to output file to the same folder
 		String[] part1 = modelPath.split("/");
@@ -54,21 +50,20 @@ public class StructureFromFile {
 		struct.writeToFile(outputPath + part2[0] + "_solution_" + df.format(date) + ".txt");
 
 		// overwrite scale values
-		/*
-		 * viz.setRadiusScale(2); viz.setConstraintScale(0.6);
-		 * viz.setArrowShaftScale(1e-3); viz.setArrowRadiusScale(1e-1);
-		 * viz.setDisplacementScale(1e2); viz.setElementForceScale(1e-6);
-		 */
+		viz.setRadiusScale(2); viz.setConstraintScale(0.6);
+		viz.setArrowShaftScale(1e-3); viz.setArrowRadiusScale(1e-1);
+		viz.setDisplacementScale(1e2); viz.setElementForceScale(1e-6);
 
-		// viz.setNodeScale(2e-1);
+
+		viz.setNodeScale(2e-1);
 
 		// draw elements and solution
-		// viz.drawElements();
-		// viz.drawConstraints();
-		// viz.drawForces();
-		// viz.drawNodes();
-		// viz.drawDisplacements();
-		// viz.drawElementForces();
-		// viewer.setVisible(true);
+		viz.drawElements();
+		viz.drawConstraints();
+		viz.drawForces();
+		viz.drawNodes();
+		viz.drawDisplacements();
+		viz.drawElementForces();
+		viewer.setVisible(true);
 	}
 }
